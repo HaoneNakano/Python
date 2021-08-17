9f1bb8f3a5f6db6d1595813f74bd272f7882ff05
Haone Nakano <haonenakano@hotmail.com>
2021-08-17 09:30:06 -0300

    divination numver secret game

--- Python/divination.py
+++ Python/divination.py
@@ -3,15 +3,15 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
-total_tentativas = 3
-rodada = 1
+total_de_tentativas = 3
 
-while (rodada <= total_tentativas):
-    print("Tentativa: ", rodada, "de", total_tentativas)
-    chute = int(input("Digite o seu número: "))
-    print("Voce digitou ", chute)
+for rodada in range(1, total_de_tentativas + 1):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite o seu número: ")
+    print("Você digitou: ", chute_str)
+    chute = int(chute_str)
 
-    acertou = chute == numero_secreto
+    acertou = numero_secreto == chute
     maior = chute > numero_secreto
     menor = chute < numero_secreto
 
@@ -22,7 +22,5 @@ while (rodada <= total_tentativas):
             print("Você errou! O seu chute foi maior que o número secreto.")
         elif (menor):
             print("Você errou! O seu chute foi menor que o número secreto.")
-    rodada = rodada + 1
-
 
 print("Fim do jogo")
