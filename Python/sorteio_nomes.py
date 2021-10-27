6f5eb1f94cd0a1c1d2413a3fef54866f695e5ff8
Haone Nakano <haonenakano@hotmail.com>
2021-10-27 12:15:36 -0300

    new features

---
+++ Python/sorteio_nomes.py
@@ -0,0 +1,22 @@
+# inserir nomes dos funcionários e fazer um sorteio de nomes
+def main():
+    nomes = []
+    nome = input("Digite o nome do funcionário: ")
+    while nome != "":
+        nomes.append(nome)
+        nome = input("Digite o nome do funcionário: ")
+    formato = input("Deseja sortear em dupla? (S/N) ")
+    if formato == "S":
+        sorteio(nomes)
+        sorteio(nomes)
+    else:
+        sorteio(nomes)
+
+
+def sorteio(nomes):
+    import random
+    sorteado = random.choice(nomes)
+    print("O funcionário sorteado foi: ", sorteado)
+
+
+main()
